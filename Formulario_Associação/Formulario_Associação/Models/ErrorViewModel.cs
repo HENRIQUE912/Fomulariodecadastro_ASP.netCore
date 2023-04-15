a40b7acfed69af49182487e3d81d83156239dd6f
Henrique <85001190+HENRIQUE912@users.noreply.github.com>
2023-04-15 10:47:46 -0300

    Parte 1 do formulario

---
+++ Formulario_Associação/Formulario_Associação/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Formulario_Associação.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
